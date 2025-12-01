06732972d866e9dc3d3e49950af85f088c0a6d8a
henrytnk <henrytnk@gmail.com>
2025-12-01 18:05:02 -0300

    Add Typescript

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,16 @@
 import express from 'express';
+import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Tanaka Library is live.');
-})
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}.`);
-})
+});
